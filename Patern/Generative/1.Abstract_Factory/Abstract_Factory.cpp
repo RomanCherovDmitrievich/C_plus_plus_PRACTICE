a46741fa50e128131185ad1c27966a45150b803b
Roman Chernov <roma@chernov.cloud>
2025-08-07 06:27:58 +0300

    Добавление коминтариев в патерн Абстрактная фабрика

--- Patern/Generative/1.Abstract_Factory/Abstract_Factory.cpp
+++ Patern/Generative/1.Abstract_Factory/Abstract_Factory.cpp
@@ -3,6 +3,29 @@
 #include <string>
 #include <stdexcept>
 
+// Проблема 1: Если надо будет добавить много продуктов?
+// Ответ 1: Просто добавляем реализацию этого орудия к нужному кузнецу, добавляем функцию которая проверяет,
+// может ли этот кузнец сделать это орудие, делаем функцию в Client чтоб у него был доступ к новому орудию у кузнецов.
+// Вывод: Решение удобное для этого кода, так как не надо сильно менять код. +
+
+// Проблема 2: Если надо будет добавить много кузнецов?
+// Ответ 2: Добавляем новый интерфейс и его класс где будут реализованы его функции, добавляем новую фабрику к нашей абстрактной фабрике, 
+// в которой указываем ссылку на нужный вам класс
+// Вывод: Решение относительно удобное, ведь происходит много копирования, если каждый кузнец будет способен делать только 1  индивидуальный продукт,
+// то придётся делать реализацию того, что этот кузнец не может делать другие типы продуктов. -
+
+// Проблема 3: Если определённый кузнец научиться делать тот продукт, который раньше не мог?
+// Ответ 3: Просто добавляем функцию в реализацию кузнеца, меняем проверку так, чтоб клиент увидел что кузнец теперь может делать этот продукт.
+// Вывод: Решение удобное для этого кода, так как не надо сильно менять код. +
+
+// Проблема 4: Может ли один кузнец создавать продукты другого кузнеца(Например японский кузнец научился делать русский меч)?
+// Ответ 4: Добавляем новую функцию в реализацию конкретного кузнеца.
+// Вывод: Классы кузнецов не связаны напрямую между собой, а значит пользоваться умениями другого не получиться, придётся добавлять новый продукт в его интерфейс и делать его реализацию
+
+// Пользователь видит только класс клиента, и доступа к другим классам у него нет, мы скрыли реализацию по созданию наших продуктов.
+
+
+// Product
 class IArsenal
 {
 public:
@@ -12,7 +35,6 @@ public:
     {
     public:
         virtual void createArsenal() const = 0;
-        //virtual void createArmor() const = 0;
         virtual ~IRussianArsenal() = default;
     };
 
@@ -25,8 +47,8 @@ public:
 
     virtual std::unique_ptr<IRussianArsenal> createRusArsenal(const std::string& type_arsenal) const = 0;
     virtual std::unique_ptr<IJapaneseArsenal> createJapArsenal(const std::string& type_arsenal) const = 0;
-    virtual bool can_create_sword(const std::string& type_sword) const = 0;
-    virtual bool can_create_armor(const std::string& type_armor) const = 0; 
+    virtual bool can_create_swords(const std::string& type_sword) const = 0;
+    virtual bool can_create_armors(const std::string& type_armor) const = 0; 
 };
 
 class RussianBlacksmith : public IArsenal // Конкретный класс русского арсенала
@@ -68,11 +90,11 @@ public:
         throw std::invalid_argument(type_arsenal + "не может сделать");
     }
 
-    bool can_create_sword(const std::string& type_sword) const override
+    bool can_create_swords(const std::string& type_sword) const override
     {
         return type_sword == "sword";
     }
-    bool can_create_armor(const std::string& type_armor) const override
+    bool can_create_armors(const std::string& type_armor) const override
     {
         return type_armor == "armor";
     }
@@ -104,7 +126,7 @@ public:
         
     }
 
-    bool can_create_sword(const std::string& type_sword) const override
+    bool can_create_swords(const std::string& type_sword) const override 
     {
         if(type_sword == "sword")
         {
@@ -115,13 +137,13 @@ public:
             return false;
         }
     }
-    bool can_create_armor(const std::string& type_armor) const override
+    bool can_create_armors(const std::string& type_armor) const override
     {
         return type_armor == "armor";
     }
 };
 
-////////////////
+// Factory
 class AbstractionFactoryBlacksmiths
 {
 public:
@@ -172,14 +194,14 @@ public:
         std::unique_ptr<AbstractionFactoryBlacksmiths> factory = createFactory(type_blacksmith); // FactoryRussianBlacksmith
         if(!factory)
         {
-            std::cout << "Ещё не существует " << type_blacksmith << " кузнец."; 
+            std::cout << "Ещё не существует " << type_blacksmith << " кузнеца."; 
             return ;
         }
         auto blacksmith = factory->createBlacksmith(); //RussianBlacksmith
 
         try
         {
-            if(blacksmith->can_create_armor(type_arsenal)) // armor (true)
+            if(blacksmith->can_create_armors(type_arsenal)) // armor (true)
             {
                 if(type_blacksmith == "japanese")
                 {
@@ -194,7 +216,7 @@ public:
             }
             
 
-            if(blacksmith->can_create_sword(type_arsenal)) // sword (true)
+            if(blacksmith->can_create_swords(type_arsenal)) // sword (true)
             {
                 if(type_blacksmith == "japanese")
                 {
@@ -207,7 +229,7 @@ public:
                 }
             }
 
-            if(!blacksmith->can_create_armor(type_arsenal)  && !blacksmith->can_create_sword(type_arsenal))
+            if(!blacksmith->can_create_armors(type_arsenal)  && !blacksmith->can_create_swords(type_arsenal))
             {
                std::cout << type_blacksmith << " кузнец"  << " не может сделать " << type_arsenal;
             }
